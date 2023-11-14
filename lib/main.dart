674bcde5fec0de5a147052cad64d0b6f47a13e20
tinhkait <lamtrungtinhcntt2016@gmail.com>
2023-11-14 22:34:56 +0700

    giao dien

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,101 @@
-import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:fooddelivery_fe/screens/homescreen/homescreen.dart';
+import 'dart:io';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+
+import 'package:fooddelivery_fe/screens/login/welcome_view.dart';
+import 'package:fooddelivery_fe/screens/main_tabview/main_tabview.dart';
+import 'package:fooddelivery_fe/screens/on_boarding/startup_view.dart';
+import 'package:fooddelivery_fe/utils/custom/color_extension.dart';
+import 'package:fooddelivery_fe/utils/custom/globs.dart';
+import 'package:fooddelivery_fe/utils/custom/locator.dart';
+import 'package:fooddelivery_fe/utils/custom/my_http_overrides.dart';
+import 'package:fooddelivery_fe/utils/custom/service_call.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+SharedPreferences? prefs;
 void main() async {
+  setUpLocator();
+  HttpOverrides.global = MyHttpOverrides();
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
-  // MainController.initializeControllers();
-  // await initializeDateFormatting('vi_VN', null);
-  runApp(
-    EasyLocalization(
-      supportedLocales: const [Locale('en', 'US'), Locale('vi', 'VN')],
-      path: 'assets/translations', //
-      fallbackLocale: const Locale('en', 'US'),
-      startLocale: const Locale('vi', 'VN'),
-      child: const ScreenUtilInit(
-          designSize: Size(375, 812),
-          minTextAdapt: true,
-          splitScreenMode: true,
-          child: AppFood()),
-    ),
-  );
+  prefs = await SharedPreferences.getInstance();
+
+  if (Globs.udValueBool(Globs.userLogin)) {
+    ServiceCall.userPayload = Globs.udValue(Globs.userPayload);
+  }
+
+  runApp(const MyApp(
+    defaultHome: StartupView(),
+  ));
 }
 
-class AppFood extends StatelessWidget {
-  const AppFood({super.key});
+void configLoading() {
+  EasyLoading.instance
+    ..indicatorType = EasyLoadingIndicatorType.ring
+    ..loadingStyle = EasyLoadingStyle.custom
+    ..indicatorSize = 45.0
+    ..radius = 5.0
+    ..progressColor = TColor.primaryText
+    ..backgroundColor = TColor.primary
+    ..indicatorColor = Colors.yellow
+    ..textColor = TColor.primaryText
+    ..userInteractions = false
+    ..dismissOnTap = false;
+}
 
+class MyApp extends StatefulWidget {
+  final Widget defaultHome;
+  const MyApp({super.key, required this.defaultHome});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: context.locale,
-      initialRoute: 'home_screen',
+      title: 'Food Delivery',
       debugShowCheckedModeBanner: false,
-      routes: {
-        'home_screen': (context) => HomeScreen(),
+      theme: ThemeData(
+        fontFamily: "Metropolis",
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        // useMaterial3: true,
+      ),
+      home: widget.defaultHome,
+      navigatorKey: locator<NavigationService>().navigatorKey,
+      onGenerateRoute: (routeSettings) {
+        switch (routeSettings.name) {
+          case "welcome":
+            return MaterialPageRoute(builder: (context) => const WelcomeView());
+          case "Home":
+            return MaterialPageRoute(builder: (context) => const MainTabView());
+          default:
+            return MaterialPageRoute(
+                builder: (context) => Scaffold(
+                      body: Center(
+                          child: Text("No path for ${routeSettings.name}")),
+                    ));
+        }
+      },
+      builder: (context, child) {
+        return FlutterEasyLoading(child: child);
       },
     );
   }
---
+++ lib/screens/login/welcome_view.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+import 'package:fooddelivery_fe/screens/login/login_view.dart';
+import 'package:fooddelivery_fe/screens/login/sing_up_view.dart';
+import 'package:fooddelivery_fe/utils/custom/color_extension.dart';
+import 'package:fooddelivery_fe/widgets/round_button.dart';
+
+class WelcomeView extends StatefulWidget {
+  const WelcomeView({super.key});
+
+  @override
+  State<WelcomeView> createState() => _WelcomeViewState();
+}
+
+class _WelcomeViewState extends State<WelcomeView> {
+  @override
+  Widget build(BuildContext context) {
+    var media = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                Image.asset(
+                  "assets/img/welcome_top_shape.png",
+                  width: media.width,
+                ),
+                Image.asset(
+                  "assets/img/app_logo_datfood.png",
+                  width: media.width * 0.55,
+                  height: media.width * 0.55,
+                  fit: BoxFit.contain,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: media.width * 0.1,
+            ),
+            Text(
+              "Khám phá những món ăn tuyệt vời từ hơn 1.000\nnhà hàng và được giao hàng nhanh chóng đến\ncửa nhà của bạn.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: TColor.secondaryText,
+                  fontSize: 13,
+                  fontWeight: FontWeight.w500),
+            ),
+            SizedBox(
+              height: media.width * 0.1,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: RoundButton(
+                title: "Login",
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const LoginView(),
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: RoundButton(
+                title: "Create an Account",
+                type: RoundButtonType.textPrimary,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SignUpView(),
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
